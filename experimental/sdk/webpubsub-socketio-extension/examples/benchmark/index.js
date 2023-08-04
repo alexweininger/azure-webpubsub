f30dec1c02cb96b4053acec4117ade1d590d2fb1
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2023-08-04 19:30:47 +0800

    add benchmark tool

---
+++ experimental/sdk/webpubsub-socketio-extension/examples/benchmark/index.js
@@ -0,0 +1,48 @@
+const wpsExt = require("@azure/web-pubsub-socket.io")
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')
+const { instrument } = require("@socket.io/admin-ui");
+
+const wpsOptions = {
+  hub: "eio_hub",
+  connectionString: process.argv[2] || process.env.WebPubSubConnectionString,
+}
+
+const samplingInterval = 20;
+
+async function main() {
+    const server = await io(http).useAzureSocketIO(wpsOptions)
+    instrument(server, { auth: false, mode: "production", });
+    const benchmarkNs = server.of("/benchmark");
+
+    var lastReceivedIndex = 0;
+
+    // Client -> Server time
+    var min = 10000, max = 0, sum = 0;
+    
+    benchmarkNs.on('connection', (socket) => {
+        socket.on('client to server event', (data) => {
+            var index = data.split(",")[0];
+            var cost = new Date().getTime() - data.split(",")[1];
+
+            min = Math.min(min, cost);
+            max = Math.max(max, cost);
+            sum += cost;
+            if (index % samplingInterval == 0) {
+                console.log(`client -> Server (last=#${index}) | min: ${min} ms | max: ${max} ms | avg: ${sum / samplingInterval} ms`);
+                min = 10000, max = 0, sum = 0;
+            }
+            socket.emit("server to client event", (data));
+
+            lastReceivedIndex = index;
+        });
+    });
+
+    server.httpServer.listen(3000, () => {
+        console.log('Visit http://localhost:%d', 3000);
+    });
+}
+
+main()
